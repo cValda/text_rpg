f6f4de7f03c55d6860320c870bc655be17abb9f4
cValda <p.valosek@gmail.com>
2020-03-24 21:51:42 +0100

    first functional version - 4 rooms, no items

--- base.py
+++ base.py
@@ -5,18 +5,50 @@ from classes import *
 specs = start()
 
 #defines player character
-hero = Player(specs[0], specs[1], [], 0)
+hero = Player(specs[0], specs[1], [], (0, 0))
+
+#creating items
+#weapons
+sword = Weapon('a sharp sword', 1, 10, 5, 0.7)
+
+#armor
+gambeson = Armor('a thick gambeson', 2, 5, 2, 0.9)
+
+#other
+lantern = Item('a lantern that can be lit to illuminate dark places', 1, 3)
 
 #creating rooms
-hall = Room('You\'re in a large hallway.', 'There\'s nothing here.')
+hall = Room('You\'re in a large hallway.', [sword, lantern])
 bedroom = Room('You\'re in a wealthy-looking bedroom.', 'There\'s nothing here.')
 kitchen = Room('You\'re in the kitchen.', 'There\'s nothing here.')
-living_room = ('You\'re in a comfortable living room.', 'There\'s nothing here.')
+living_room = Room('You\'re in a comfortable living room.', 'There\'s nothing here.')
 
+#indexing rooms
+rooms = {(0, 0): hall, (0, 1): bedroom, (1, 0): living_room, (1, 1): kitchen}
 
-rooms = {(0, 0): hall, (0)}
-
-
+#current location description and prompt
 while True:
-    
+    try:
+        print(rooms[hero.location].description)
+        command = input("What do you do? ")
+
+        #defining movement
+        if 'go' in command:
+            current = hero.location
+            if 'north' in command:
+                hero.location = (hero.location[0], hero.location[1] + 1)
+                print(hero.location)
+            if 'east' in command:
+                hero.location = (hero.location[0] + 1, hero.location[1])
+                print(hero.location)
+            if 'west' in command:
+                hero.location = (hero.location[0] - 1, hero.location[1])
+                print(hero.location)
+            if 'south' in command:
+                hero.location = (hero.location[0], hero.location[1] - 1)
+                print(hero.location)
+    except KeyError:
+        print("You can\'t go there!")
+        hero.location = current
+        continue
 
--- classes.py
+++ classes.py
@@ -11,16 +11,19 @@ class Room:
         self.items = items
 
 class Item:
-    def __init__(self, weight, value):
+    def __init__(self, description, weight, value):
+        self.description = description
         self.weight = weight
         self.value = value
 
 class Weapon(Item):
-    def __init__(self, damage, accuracy):
+    def __init__(self, description, weight, value, damage, accuracy):
+        super().__init__(description, weight, value)
         self.damage = damage
         self.accuracy = accuracy
 
 class Armor(Item):
-    def __init__(self, protection, speed):
+    def __init__(self, description, weight, value, protection, speed):
+        super().__init__(description, weight, value)
         self.protection = protection
         self.speed = speed
